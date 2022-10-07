dd6c3047ebca8c36a77e457253a49a8fc03f5a3d
Fausto G. Cintra <professor@faustocintra.com.br>
2022-10-07 20:08:23 -0300

    (07/10) Término da implementação da primeira classe

--- 12_classe.py
+++ 12_classe.py
@@ -6,6 +6,7 @@
     criados a partir da mesma classe terão sempre o formato determinado por
     esta.
 """
+from math import pi
 
 class FormaGeometrica:
 
@@ -20,23 +21,57 @@ class FormaGeometrica:
     # ~> variáveis passam a ser chamadas de ATRIBUTOS
 
     def __init__(self, base, altura, tipo):
-        # Validando o valor da base
-        if type(base) not in [int, float] or base <= 0:
-            raise Exception(f"ERRO: a base ({base}) deve ser numérica e maior que zero.")
-
-        # Validando o valor da altura
-        if type(altura) not in [int, float] or altura <= 0:
-            raise Exception(f"ERRO: a altura ({altura}) deve ser numérica e maior que zero.")
-
-        # Validando o tipo
-        if tipo not in ["R", "T", "E"]:
-            raise Exception(f"ERRO: o tipo ({tipo}) deve igual a R, T ou E.")
-
         # Armazenando os dados recebidos DENTRO do objeto
-        self.base = base
-        self.altura = altura
-        self.tipo = tipo
-        self.template = f"base={self.base}, altura={self.altura}, tipo={self.tipo}"
+        self.__base = base
+        #self.__altura = altura
+        self.set_altura(altura)
+        self.__tipo = tipo
+
+    # Método setter para o atributo __base
+    def set_base(self, val):
+        # Validando o valor da base
+        if type(val) not in [int, float] or val <= 0:
+            raise Exception(f"ERRO: a base ({val}) deve ser numérica e maior que zero.")
+        self.__base = val
+
+    # Método setter para o atributo __altura
+    def set_altura(self, val):
+        # Validando o valor da altura
+        if type(val) not in [int, float] or val <= 0:
+            raise Exception(f"ERRO: a altura ({val}) deve ser numérica e maior que zero.")
+        self.__altura = val
+
+    # Método setter para o atributo __tipo
+    def set_tipo(self, val):
+        # Validando o tipo
+        if val not in ["R", "T", "E"]:
+            raise Exception(f"ERRO: o tipo ({val}) deve igual a R, T ou E.")
+        self.__tipo = val
+
+    # Método getter para o atributo __base
+    def get_base(self):
+        return self.__base
+    
+    # Método getter para o atributo __altura
+    def get_altura(self):
+        return self.__altura
+
+    # Método getter para o atributo __tipo
+    def get_tipo(self):
+        return self.__tipo
+
+    # Método que calcula a área da forma geométrica
+    def calc_area(self):
+        if(self.__tipo == "R"):
+            return self.__base * self.__altura
+        if(self.__tipo == "T"):
+            return self.__base * self.__altura / 2
+        if(self.__tipo == "E"):
+            return (self.__base / 2) * (self.__altura / 2) * pi
+
+    # Método que gera uma representação de um objeto como string
+    def __str__(self):
+        return f"base={self.__base}, altura={self.__altura}, tipo={self.__tipo}"
 
 ################################################################
 
@@ -46,11 +81,15 @@ forma2 = FormaGeometrica(1.5, 3.5, 'T')
 forma3 = FormaGeometrica(4, 4.2, 'E')
 # forma4 = FormaGeometrica(-3, "tomate", "X")
 
-forma1.altura = -7
-
-print(f"forma1: {forma1.template}")
-print(f"forma2: {forma2.template}")
-print(f"forma3: {forma3.template}")
+print(f"forma1: {forma1}")
+print(f"forma2: {forma2}")
+print(f"forma3: {forma3}")
 # print(f"forma4: {forma4.template}")
 
-print('Altura atualizada de forma1: ', forma1.altura)
+forma1.set_altura(9)
+
+print('Altura atualizada de forma1: ', forma1.get_altura())
+
+print(f'forma1: {forma1}, área => {forma1.calc_area()}')
+print(f'forma1: {forma2}, área => {forma2.calc_area()}')
+print(f'forma1: {forma3}, área => {forma3.calc_area()}')
